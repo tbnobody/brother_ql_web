d087a21d9c6991b55bf38cae09e004dc2207d96a
Thomas Basler <github.thomas@familie-basler.net>
2020-05-04 17:02:39 +0200

    Move label generation logic into own class

---
+++ app/labeldesigner/label.py
@@ -0,0 +1,204 @@
+from enum import Enum, auto
+from qrcode import QRCode, constants
+from PIL import Image, ImageDraw, ImageFont
+
+
+class LabelContent(Enum):
+    TEXT_ONLY = auto()
+    QRCODE_ONLY = auto()
+    TEXT_QRCODE = auto()
+    IMAGE = auto()
+
+
+class LabelOrientation(Enum):
+    STANDARD = auto()
+    ROTATED = auto()
+
+
+class LabelType(Enum):
+    ENDLESS_LABEL = auto()
+    DIE_CUT_LABEL = auto()
+    ROUND_DIE_CUT_LABEL = auto()
+
+
+class TextAlign(Enum):
+    LEFT = 'left'
+    CENTER = 'center'
+    RIGHT = 'right'
+
+
+class SimpleLabel:
+    qr_correction_mapping = {
+        'L': constants.ERROR_CORRECT_L,
+        'M': constants.ERROR_CORRECT_M,
+        'Q': constants.ERROR_CORRECT_Q,
+        'H': constants.ERROR_CORRECT_H
+    }
+
+    def __init__(
+            self,
+            width=0,
+            height=0,
+            label_content=LabelContent.TEXT_ONLY,
+            label_orientation=LabelOrientation.STANDARD,
+            label_type=LabelType.ENDLESS_LABEL,
+            label_margin=(0, 0, 0, 0),  # Left, Right, Top, Bottom
+            fore_color=(0, 0, 0),  # Red, Green, Blue
+            text='',
+            text_align=TextAlign.CENTER,
+            qr_size=10,
+            qr_correction='L',
+            image=None,
+            font_path='',
+            font_size=70,
+            line_spacing=100):
+        self._width = width
+        self._height = height
+        self.label_content = label_content
+        self._label_orientation = label_orientation
+        self._label_type = label_type
+        self._label_margin = label_margin
+        self._fore_color = fore_color
+        self._text = text
+        self._text_align = text_align
+        self._qr_size = qr_size
+        self.qr_correction = qr_correction
+        self._image = image
+        self._font_path = font_path
+        self._font_size = font_size
+        self._line_spacing = line_spacing
+
+    @property
+    def label_content(self):
+        return self._label_content
+
+    @label_content.setter
+    def label_content(self, value):
+        self._label_content = value
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, value):
+        self._text = value
+
+    @property
+    def qr_correction(self):
+        for key, val in self.qr_correction_mapping:
+            if val == self._qr_correction:
+                return key
+
+    @qr_correction.setter
+    def qr_correction(self, value):
+        self._qr_correction = self.qr_correction_mapping.get(
+            value, constants.ERROR_CORRECT_L)
+
+    def generate(self):
+        if self._label_content in (LabelContent.QRCODE_ONLY, LabelContent.TEXT_QRCODE):
+            img = self._generate_qr()
+        elif self._label_content == LabelContent.IMAGE:
+            img = self._image
+        else:
+            img = None
+
+        if img is not None:
+            img_width, img_height = img.size
+        else:
+            img_width, img_height = (0, 0)
+
+        if self._label_content in (LabelContent.TEXT_ONLY, LabelContent.TEXT_QRCODE):
+            textsize = self._get_text_size()
+        else:
+            textsize = (0, 0)
+
+        width, height = self._width, self._height
+        margin_left, margin_right, margin_top, margin_bottom = self._label_margin
+
+        if self._label_orientation == LabelOrientation.STANDARD:
+            if self._label_type in (LabelType.ENDLESS_LABEL,):
+                height = img_height + textsize[1] + margin_top + margin_bottom
+        elif self._label_orientation == LabelOrientation.ROTATED:
+            if self._label_type in (LabelType.ENDLESS_LABEL,):
+                width = img_width + textsize[0] + margin_left + margin_right
+
+        if self._label_orientation == LabelOrientation.STANDARD:
+            if self._label_type in (LabelType.DIE_CUT_LABEL, LabelType.ROUND_DIE_CUT_LABEL):
+                vertical_offset_text = (height - img_height - textsize[1])//2
+                vertical_offset_text += (margin_top - margin_bottom)//2
+            else:
+                vertical_offset_text = margin_top
+
+            vertical_offset_text += img_height
+            horizontal_offset_text = max((width - textsize[0])//2, 0)
+            horizontal_offset_image = (width - img_width)//2
+            vertical_offset_image = margin_top
+
+        elif self._label_orientation == LabelOrientation.ROTATED:
+            vertical_offset_text = (height - textsize[1])//2
+            vertical_offset_text += (margin_top - margin_bottom)//2
+            if self._label_type in (LabelType.DIE_CUT_LABEL, LabelType.ROUND_DIE_CUT_LABEL):
+                horizontal_offset_text = max((width - img_width - textsize[0])//2, 0)
+            else:
+                horizontal_offset_text = margin_left
+            horizontal_offset_text += img_width
+            horizontal_offset_image = margin_left
+            vertical_offset_image = (height - img_height)//2
+
+        text_offset = horizontal_offset_text, vertical_offset_text
+        image_offset = horizontal_offset_image, vertical_offset_image
+
+        imgResult = Image.new('RGB', (width, height), 'white')
+
+        if img is not None:
+            imgResult.paste(img, image_offset)
+
+        if self._label_content in (LabelContent.TEXT_ONLY, LabelContent.TEXT_QRCODE):
+            draw = ImageDraw.Draw(imgResult)
+            draw.multiline_text(
+                text_offset,
+                self._prepare_text(self._text),
+                self._fore_color,
+                font=self._get_font(),
+                align=self._text_align,
+                spacing=int(self._font_size*((self._line_spacing - 100) / 100)))
+
+        return imgResult
+
+    def _generate_qr(self):
+        qr = QRCode(
+            version=1,
+            error_correction=self._qr_correction,
+            box_size=self._qr_size,
+            border=0,
+        )
+        qr.add_data(self._text)
+        qr.make(fit=True)
+        qr_img = qr.make_image(
+            fill_color='red' if (255, 0, 0) == self._fore_color else 'black',
+            back_color="white")
+        return qr_img
+
+    def _get_text_size(self):
+        font = self._get_font()
+        img = Image.new('L', (20, 20), 'white')
+        draw = ImageDraw.Draw(img)
+        return draw.multiline_textsize(
+            self._prepare_text(self._text),
+            font=font,
+            spacing=int(self._font_size*((self._line_spacing - 100) / 100)))
+
+    @staticmethod
+    def _prepare_text(text):
+        # workaround for a bug in multiline_textsize()
+        # when there are empty lines in the text:
+        lines = []
+        for line in text.split('\n'):
+            if line == '':
+                line = ' '
+            lines.append(line)
+        return '\n'.join(lines)
+
+    def _get_font(self):
+        return ImageFont.truetype(self._font_path, self._font_size)
--- app/labeldesigner/routes.py
+++ app/labeldesigner/routes.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import current_app, render_template, request, make_response
-from PIL import Image, ImageDraw, ImageFont
 
 from brother_ql.devicedependent import label_type_specs, label_sizes
 from brother_ql.devicedependent import ENDLESS_LABEL, DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL
@@ -12,6 +11,8 @@ from app.utils import convert_image_to_bw, pdffile_to_image, imgfile_to_image, i
 from app import BACKEND_CLASS, FONTS
 
 import qrcode
+
+from .label import SimpleLabel, LabelContent, LabelOrientation, LabelType
 
 LINE_SPACINGS = (100, 150, 200, 250, 300)
 
@@ -51,8 +52,8 @@ def get_font_styles():
 
 @bp.route('/api/preview', methods=['POST', 'GET'])
 def get_preview_from_image():
-    context = get_label_context(request)
-    im = create_label_im(**context)
+    label = genreate_label_from_request(request)
+    im = label.generate()
 
     return_format = request.values.get('return_format', 'png')
 
@@ -82,6 +83,7 @@ def print_text():
 
     try:
         context = get_label_context(request)
+        label = genreate_label_from_request(request)
     except LookupError as e:
         return_dict['error'] = e.msg
         return return_dict
@@ -90,7 +92,7 @@ def print_text():
         return_dict['error'] = 'Please provide the text for the label'
         return return_dict
 
-    im = create_label_im(**context)
+    im = label.generate()
 
     if context['kind'] == ENDLESS_LABEL:
         rotate = 0 if context['orientation'] == 'standard' else 90
@@ -133,53 +135,44 @@ def print_text():
     return return_dict
 
 
-def get_label_context(request):
-    """ might raise LookupError() """
-
-    d = request.values  # UTF-8 decoded form data
-
+def genreate_label_from_request(request):
+    d = request.values
     context = {
-        'text':          d.get('text', None),
-        'font_size': int(d.get('font_size', 100)),
-        'font_family':   d.get('font_family'),
-        'font_style':    d.get('font_style'),
-        'label_size':    d.get('label_size', "62"),
-        'kind':          label_type_specs[d.get('label_size', "62")]['kind'],
-        'margin':    int(d.get('margin', 10)),
-        'threshold': int(d.get('threshold', 70)),
-        'align':         d.get('align', 'center'),
-        'orientation':   d.get('orientation', 'standard'),
-        'margin_top':    float(d.get('margin_top',    24))/100.,
+        'label_size': d.get('label_size', '62'),
+        'print_type': d.get('print_type', 'text'),
+        'label_orientation': d.get('orientation', 'standard'),
+        'kind': label_type_specs[d.get('label_size', "62")]['kind'],
+        'margin_top': float(d.get('margin_top', 24))/100.,
         'margin_bottom': float(d.get('margin_bottom', 45))/100.,
-        'margin_left':   float(d.get('margin_left',   35))/100.,
-        'margin_right':  float(d.get('margin_right',  35))/100.,
-        'print_type':    d.get('print_type', 'text'),
-        'qrcode_size':   int(d.get('qrcode_size', 10)),
+        'margin_left': float(d.get('margin_left', 35))/100.,
+        'margin_right': float(d.get('margin_right', 35))/100.,
+        'text': d.get('text', None),
+        'align': d.get('align', 'center'),
+        'qrcode_size': int(d.get('qrcode_size', 10)),
         'qrcode_correction': d.get('qrcode_correction', 'L'),
-        'print_count':       int(d.get('print_count', 1)),
-        'print_color':       d.get('print_color', 'black'),
-        'line_spacing':      int(d.get('line_spacing', 100)),
-        'cut_once':          int(d.get('cut_once', 0)),
+        'font_size': int(d.get('font_size', 100)),
+        'line_spacing': int(d.get('line_spacing', 100)),
+        'font_family': d.get('font_family'),
+        'font_style': d.get('font_style'),
+        'print_color': d.get('print_color', 'black'),
     }
-    context['margin_top'] = int(context['font_size']*context['margin_top'])
-    context['margin_bottom'] = int(
-        context['font_size']*context['margin_bottom'])
-    context['margin_left'] = int(context['font_size']*context['margin_left'])
-    context['margin_right'] = int(context['font_size']*context['margin_right'])
 
-    context['fill_color'] = (
-        255, 0, 0) if 'red' in context['label_size'] and context['print_color'] == 'red' else (0, 0, 0)
+    def get_label_dimensions(label_size):
+        try:
+            ls = label_type_specs[context['label_size']]
+        except KeyError:
+            raise LookupError("Unknown label_size")
+        return ls['dots_printable']
 
-    context['cut_once'] = True if context['cut_once'] == 1 else False
-
-    qrSwitch = {
-        'L': qrcode.constants.ERROR_CORRECT_L,
-        'M': qrcode.constants.ERROR_CORRECT_M,
-        'Q': qrcode.constants.ERROR_CORRECT_Q,
-        'H': qrcode.constants.ERROR_CORRECT_H
-    }
-    context['qrcode_correction'] = qrSwitch.get(
-        context['qrcode_correction'], qrcode.constants.ERROR_CORRECT_L)
+    def get_font_path(font_family_name, font_style_name):
+        try:
+            if font_family_name is None or font_style_name is None:
+                font_family_name = current_app.config['LABEL_DEFAULT_FONT_FAMILY']
+                font_style_name = current_app.config['LABEL_DEFAULT_FONT_STYLE']
+            font_path = FONTS.fonts[font_family_name][font_style_name]
+        except KeyError:
+            raise LookupError("Couln't find the font & style")
+        return font_path
 
     def get_uploaded_image(image):
         try:
@@ -195,152 +188,74 @@ def get_label_context(request):
         except AttributeError:
             return None
 
-    context['upload_image'] = get_uploaded_image(
-        request.files.get('image', None))
+    if context['print_type'] == 'text':
+        label_content = LabelContent.TEXT_ONLY
+    elif context['print_type'] == 'qrcode':
+        label_content = LabelContent.QRCODE_ONLY
+    elif context['print_type'] == 'qrcode_text':
+        label_content = LabelContent.TEXT_QRCODE
+    else:
+        label_content = LabelContent.IMAGE
 
-    def get_font_path(font_family_name, font_style_name):
-        try:
-            if font_family_name is None or font_style_name is None:
-                font_family_name = current_app.config['LABEL_DEFAULT_FONT_FAMILY']
-                font_style_name = current_app.config['LABEL_DEFAULT_FONT_STYLE']
-            font_path = FONTS.fonts[font_family_name][font_style_name]
-        except KeyError:
-            raise LookupError("Couln't find the font & style")
-        return font_path
+    if context['label_orientation'] == 'rotated':
+        label_orientation = LabelOrientation.ROTATED
+    else:
+        label_orientation = LabelOrientation.STANDARD
 
-    context['font_path'] = get_font_path(
-        context['font_family'], context['font_style'])
-
-    def get_label_dimensions(label_size):
-        try:
-            ls = label_type_specs[context['label_size']]
-        except KeyError:
-            raise LookupError("Unknown label_size")
-        return ls['dots_printable']
+    if context['kind'] == ENDLESS_LABEL:
+        label_type = LabelType.ENDLESS_LABEL
+    elif context['kind'] == DIE_CUT_LABEL:
+        label_type = LabelType.DIE_CUT_LABEL
+    else:
+        label_type = LabelType.ROUND_DIE_CUT_LABEL
 
     width, height = get_label_dimensions(context['label_size'])
     if height > width:
         width, height = height, width
-    if context['orientation'] == 'rotated':
+    if label_orientation == LabelOrientation.ROTATED:
         height, width = width, height
-    context['width'], context['height'] = width, height
+
+    return SimpleLabel(
+        width=width,
+        height=height,
+        label_content=label_content,
+        label_orientation=label_orientation,
+        label_type=label_type,
+        label_margin=(
+            int(context['font_size']*context['margin_left']),
+            int(context['font_size']*context['margin_right']),
+            int(context['font_size']*context['margin_top']),
+            int(context['font_size']*context['margin_bottom'])
+        ),
+        fore_color=
+            (255, 0, 0) if 'red' in context['label_size'] and context['print_color'] == 'red'
+            else (0, 0, 0),
+        text=context['text'],
+        text_align=context['align'],
+        qr_size=context['qrcode_size'],
+        qr_correction=context['qrcode_correction'],
+        image=get_uploaded_image(request.files.get('image', None)),
+        font_path=get_font_path(context['font_family'], context['font_style']),
+        font_size=context['font_size'],
+        line_spacing=context['line_spacing']
+    )
+
+
+def get_label_context(request):
+    """ might raise LookupError() """
+
+    d = request.values  # UTF-8 decoded form data
+
+    context = {
+        'text':          d.get('text', None),
+        'label_size':    d.get('label_size', "62"),
+        'kind':          label_type_specs[d.get('label_size', "62")]['kind'],
+        'threshold': int(d.get('threshold', 70)),
+        'orientation':   d.get('orientation', 'standard'),
+        'print_count':       int(d.get('print_count', 1)),
+        'cut_once':          int(d.get('cut_once', 0))
+    }
+
+    context['cut_once'] = True if context['cut_once'] == 1 else False
 
     return context
-
-
-def create_qr_code(text, size, correction, fill_color):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=correction,
-        box_size=size,
-        border=0,
-    )
-    qr.add_data(text)
-    qr.make(fit=True)
-    qr_img = qr.make_image(
-        fill_color='red' if (255, 0, 0) == fill_color else 'black',
-        back_color="white")
-    return qr_img
-
-
-def create_label_im(text, **kwargs):
-    if kwargs['print_type'] == 'qrcode' or kwargs['print_type'] == 'qrcode_text':
-        img = create_qr_code(
-            text,
-            kwargs['qrcode_size'],
-            kwargs['qrcode_correction'],
-            kwargs['fill_color']
-        )
-    elif kwargs['print_type'] == 'image':
-        img = kwargs['upload_image']
-    else:
-        img = None
-
-    if kwargs['print_type'] == 'qrcode':
-        return assemble_label_im(text, img, False, **kwargs)
-    elif kwargs['print_type'] == 'qrcode_text':
-        return assemble_label_im(text, img, True, **kwargs)
-    elif kwargs['print_type'] == 'image':
-        return assemble_label_im(text, img, False, **kwargs)
-    else:
-        return assemble_label_im(text, None, True, **kwargs)
-
-
-def assemble_label_im(text, image, include_text, **kwargs):
-    if image is not None:
-        image_width, image_height = image.size
-    else:
-        image_width, image_height = (0, 0)
-
-    label_type = kwargs['kind']
-
-    if include_text:
-        im_font = ImageFont.truetype(kwargs['font_path'], kwargs['font_size'])
-        im = Image.new('L', (20, 20), 'white')
-        draw = ImageDraw.Draw(im)
-        # workaround for a bug in multiline_textsize()
-        # when there are empty lines in the text:
-        lines = []
-        for line in text.split('\n'):
-            if line == '':
-                line = ' '
-            lines.append(line)
-        text = '\n'.join(lines)
-        textsize = draw.multiline_textsize(text, font=im_font, spacing=int(
-            kwargs['font_size']*((kwargs['line_spacing'] - 100) / 100)))
-    else:
-        textsize = (0, 0)
-
-    width, height = kwargs['width'], kwargs['height']
-    if kwargs['orientation'] == 'standard':
-        if label_type in (ENDLESS_LABEL,):
-            height = image_height + \
-                textsize[1] + kwargs['margin_top'] + kwargs['margin_bottom']
-    elif kwargs['orientation'] == 'rotated':
-        if label_type in (ENDLESS_LABEL,):
-            width = image_width + \
-                textsize[0] + kwargs['margin_left'] + kwargs['margin_right']
-
-    if kwargs['orientation'] == 'standard':
-        if label_type in (DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL):
-            vertical_offset = (height - image_height - textsize[1])//2
-            vertical_offset += (kwargs['margin_top'] -
-                                kwargs['margin_bottom'])//2
-        else:
-            vertical_offset = kwargs['margin_top']
-
-        vertical_offset += image_height
-        horizontal_offset = max((width - textsize[0])//2, 0)
-        horizontal_offset_image = (width - image_width)//2
-        vertical_offset_image = kwargs['margin_top']
-
-    elif kwargs['orientation'] == 'rotated':
-        vertical_offset = (height - textsize[1])//2
-        vertical_offset += (kwargs['margin_top'] - kwargs['margin_bottom'])//2
-        if label_type in (DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL):
-            horizontal_offset = max((width - image_width - textsize[0])//2, 0)
-        else:
-            horizontal_offset = kwargs['margin_left']
-        horizontal_offset += image_width
-        horizontal_offset_image = kwargs['margin_left']
-        vertical_offset_image = (height - image_height)//2
-
-    offset = horizontal_offset, vertical_offset
-    image_offset = horizontal_offset_image, vertical_offset_image
-
-    im = Image.new('RGB', (width, height), 'white')
-
-    if image is not None:
-        im.paste(image, image_offset)
-
-    if include_text:
-        draw = ImageDraw.Draw(im)
-        draw.multiline_text(
-            offset,
-            text,
-            kwargs['fill_color'],
-            font=im_font,
-            align=kwargs['align'],
-            spacing=int(kwargs['font_size']*((kwargs['line_spacing'] - 100) / 100)))
-
-    return im
